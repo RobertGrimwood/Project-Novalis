983de023ea630724ee6e8bc6c79915d231aa1bdb
Robert Grimwood <FancyBlaziken@outlook.com>
2019-05-07 11:55:28 -0500

    0.3.3

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main()
+        {
+            string opening;
+            string[] jobTitle = new string[512];
+            string[] companyName = new string[512];
+            string[] jobLocation = new string[512];
+            string[] salaryPay = new string[512];
+            string[] notes = new string[512];
+            int[] applicationDate = new int[512];
+            bool[] jobOffer = new bool[512];
+
+            while (2>1)
+            {
+               
+                Console.WriteLine("What would you like to do?: Create new Job View previous Jobs Get Help" );
+                opening = Console.ReadLine();
+                    
+            }
+        }
+    }
+}
